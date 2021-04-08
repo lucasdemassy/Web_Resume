038152b2d173503a866d3a2348b998b18f80e2a6
lbres <lucas.bres@sword-group.com>
2021-04-08 17:42:24 +0200

    Remove body3D of swup elements

--- dist/swup.js
+++ dist/swup.js
@@ -272,7 +272,7 @@ var Swup = function () {
 			animationSelector: '[class*="transition-"]',
 			linkSelector: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
 			cache: true,
-			containers: ['#swup', '#body3D'],
+			containers: ['#swup'],
 			requestHeaders: {
 				'X-Requested-With': 'swup',
 				Accept: 'text/html, application/xhtml+xml'
