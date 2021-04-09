5a372822dd92a8919fc21aa657f463d43bafef37
lbres <lucas.bres@sword-group.com>
2021-04-09 14:57:21 +0200

    WIP

---
+++ src/js/components/body3D.js
@@ -0,0 +1,50 @@
+import Component from 'gia/Component';
+import THREE from '../js/three'
+
+export default class body3D extends Component {
+    constructor(element) {
+        console.log("Hello constructor")
+        super(element);
+
+        this.options = {
+            delay: 0.3,
+        };
+    }
+
+    mount() {
+        console.log("Hello THREE")
+        let body3D = document.getElementById("body3D");
+        let scene = new THREE.Scene();
+        console.dir("body3D", body3D)
+        console.log("width", body3D.getBoundingClientRect().width, "height", body3D.getBoundingClientRect().height)
+        let camera = new THREE.PerspectiveCamera( 75, body3D.getBoundingClientRect().width / body3D.getBoundingClientRect().height, 0.1, 1000 );
+
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize( body3D.getBoundingClientRect().width, body3D.getBoundingClientRect().height );
+        body3D.appendChild( renderer.domElement );
+
+        let geometry = new THREE.BoxGeometry();
+        let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        let cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+
+        let animate = function () {
+            requestAnimationFrame( animate );
+
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            renderer.render( scene, camera );
+        };
+
+        animate();
+
+        /*let newContent = "";
+        this.element.innerText.split(' ').forEach(function (word, index) {
+            newContent += `<span><span style="animation-delay: ${ index * this.options.delay + 0.4 }s">${ word }</span></span> `;
+        }.bind(this));
+        this.element.innerHTML = newContent;*/
+    }
+}
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,9 +1,11 @@
 import Increment from './Increment';
 import SeparateText from './SeparateText';
+import body3D from './body3D';
 
 const components = {
     Increment,
     SeparateText,
+    body3D
 }
 
 export default components;
