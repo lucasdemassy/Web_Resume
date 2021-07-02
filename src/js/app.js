ff1e2a97e7d0ab951cfe571bec6eb80ada494153
lbres <lucas.bres@sword-group.com>
2021-07-02 11:57:02 +0200

    Handle the click on the access footer button

--- src/js/app.js
+++ src/js/app.js
@@ -23,3 +23,38 @@ swup.on('contentReplaced', function () {
 });
 
 console.dir(document)
+
+let AccessFooter = document.getElementById('access-footer')
+console.dir("Hello Access Footer")
+console.dir(AccessFooter)
+
+let RoundBackground = document.getElementsByClassName('round-background')[0]
+console.dir("Hello round-background")
+console.dir(RoundBackground)
+
+let linkContainer = document.getElementsByClassName('link-container')[0]
+console.dir("Hello link-Container")
+console.dir(linkContainer)
+
+//When the user click on the footer icon
+AccessFooter.addEventListener("click", (e) => HandleClickFooter(e), false);
+RoundBackground.addEventListener("click", (e) => HandleClickFooter(e), false);
+
+function HandleClickFooter(e)  {
+    {
+        console.log("Click", AccessFooter, e);
+        console.dir(linkContainer);
+        //Makes appear the different links
+        let valueBottom = linkContainer.offsetHeight + 'px';
+        console.log(valueBottom);
+        linkContainer.style.bottom = valueBottom;
+        //Drop down the footer button
+        //--> Done automatically
+
+        //Reset the bottom value of the container when the mouse leaves the container
+        linkContainer.addEventListener('mouseleave', (e) => {
+            linkContainer.style.bottom = '0px';
+        }, false);
+    }
+}
+
